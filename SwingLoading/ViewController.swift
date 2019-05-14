9d45d24609177cbdbdb06c4971bfdaf3f3c093f3
NiM NiM <topsecret808@gmail.com>
2019-05-14 10:23:44 +0700

    Initial Commit

---
+++ SwingLoading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwingLoading
+//
+//  Created by NiM Thitipariwat on 14/5/2562 BE.
+//  Copyright © 2562 NiM Thitipariwat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
